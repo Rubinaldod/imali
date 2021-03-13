6c8be99bffdc051242eb215eda775b66d70b8073
rubinaldod <rubinaldomingos@gmail.com>
2021-03-13 18:50:24 +0100

    there we go

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +8,73 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'poppins'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade900,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Login using your @sign...',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                child: TextField(
+                  decoration: new InputDecoration(
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    hintText: 'Enter your @sign',
+                    hintStyle: TextStyle(
+                      height: 1.0,
+                      color: Colors.white,
+                      letterSpacing: 3,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.tightFor(width: 400, height: 50),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        letterSpacing: 3,
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
